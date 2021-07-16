6f40cbb3d1742feda428cc04457ca20f1f8255cf
Dyslectric <tafyog@gmail.com>
2021-07-16 13:48:04 -0400

    updated build scripts to support folders in src

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-g++ -c src/*.cpp -m64 -Wall -I include && g++ *.o -o bin/release/main -l SDL2 -l SDL2_image
+g++ -c src/** -m64 -Wall -I include && g++ *.o -o bin/release/main -l SDL2 -l SDL2_image
 rm *.o
 bin/release/main
--- debug.sh
+++ debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-g++ -c src/*.cpp -m64 -g -Wall -I include && g++ *.o -o bin/debug/main -l SDL2 -l SDL2_image
+g++ -c src/** -m64 -g -Wall -I include && g++ *.o -o bin/debug/main -l SDL2 -l SDL2_image
 rm *.o
 bin/debug/main
