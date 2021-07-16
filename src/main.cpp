c2558744245b5638c1c0b7c257e4d928eaa8ddd3
Dyslectric <tafyog@gmail.com>
2021-07-16 15:06:20 -0400

    pulled input polling out of while loop to use less cpu

--- src/main.cpp
+++ src/main.cpp
@@ -12,26 +12,30 @@
 
 int main()
 {
+    // initiallize the window and the game parameters
     Window window = init::window();
     GameState state = init::game_state();
 
+    // create an event variable for user input
     SDL_Event event;
 
+    // initiallizes last screen refresh variable at current time
     double lastRefresh = tools::uptime();
 
     while(!state.quit)
     {
+        // keeps the game from advancing until the next frame
         while((tools::uptime() - lastRefresh) < DELTA)
         {
-            eventHandler(event, state);
         }
         lastRefresh = tools::uptime();
 
-        render(window, state);
+        eventHandler(event, state); // polls user input and processes game logic
+        render(window, state);      // renders the screen after user input is processed
     }
 
-    SDL_DestroyWindow(window.window);
-    SDL_Quit();
+    SDL_DestroyWindow(window.window);   // destroys the window after while loop is escaped
+    SDL_Quit();                         // cleans up subsystem intialization on exit
 
     return 0;
 }
